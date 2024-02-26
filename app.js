ddae5d587d396e84bd8c6532b9bd9b5c807065db
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2024-02-26 21:29:06 +0100

    workHomeOne

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const DB_URI = process.env.DB_URI;
 mongoose
   .connect(DB_URI)
   .then(() => console.log("Database connection successful"))
-  .catch((error) => console.error("Database connection error", error));
+  .catch((error) => {
+    console.error("Database connection error", error);
+    process.exit(1);
+  });
 
 const app = express();
 
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -7,9 +7,13 @@ import {
   upContact,
 } from "../services/contactsServices.js";
 
+import Contact from "../models/contact.js";
+
 export const getAllContacts = async (req, res, next) => {
   try {
-    res.send(await listContacts());
+    // res.send(await listContacts());
+    const contacts = await Contact.find();
+    res.send(contacts);
   } catch (error) {
     next(error);
   }
@@ -18,7 +22,11 @@ export const getAllContacts = async (req, res, next) => {
 export const getOneContact = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const contact = await getContactById(id);
+    // const contact = await getContactById(id);
+    const contact = await Contact.findById(id);
+    // if (contact === null) {
+    //   res.status(404).send("Not found");
+    // }
     if (!contact) {
       next(HttpError(404));
       return;
@@ -45,7 +53,9 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res) => {
   try {
-    res.status(201).send(await addContact(req.body));
+    // res.status(201).send(await addContact(req.body));
+    const result = await Contact.create(req.body);
+    res.status(201).send(result);
   } catch (error) {
     next(error);
   }
